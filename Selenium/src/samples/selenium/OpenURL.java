978bcf06a84dfd4874ee14e9f14fb90e37fa0cc4
SRKRaju <srk.raju@icloud.com>
2020-09-08 10:14:39 +0530

    ExternalDependenciesUpdated

--- Selenium/src/samples/selenium/OpenURL.java
+++ Selenium/src/samples/selenium/OpenURL.java
@@ -1,26 +1,19 @@
 package samples.selenium;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class OpenURL {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver", "/Users/rs683045/eclipse-workspace/extLibs/browsers/chromedriver");
-		System.setProperty("webdriver.gecko.driver",  "/Users/rs683045/eclipse-workspace/extLibs/browsers/geckodriver");
+		System.setProperty("webdriver.chrome.driver", "/Users/rs683045/git/workspace-rk/Selenium/lib/browsers/chromedriver");
 		
 		ChromeDriver chromeDriver = new ChromeDriver();
-		FirefoxDriver firefoxDriver = new FirefoxDriver();
 		
-		chromeDriver.get("http://seleniumhq.org");
-		firefoxDriver.get("https://www.facebook.com");
-
+		chromeDriver.get("http://www.facebook.com");
 		Thread.sleep(10000);
 		System.out.println("Completed Test");
 		chromeDriver.close();
-		firefoxDriver.close();
-		
-		
 		
 		
 
