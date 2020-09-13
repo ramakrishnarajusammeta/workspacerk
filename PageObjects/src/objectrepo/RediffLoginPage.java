6342c1e64a7a5c103eccba86c63f6bc39cc42f91
SRKRaju <srk.raju@icloud.com>
2020-09-13 22:29:48 +0530

    PageObject_with_PageFactory

--- PageObjects/src/objectrepo/RediffLoginPage.java
+++ PageObjects/src/objectrepo/RediffLoginPage.java
@@ -3,6 +3,9 @@ package objectrepo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class RediffLoginPage {
 	
@@ -10,21 +13,32 @@ public class RediffLoginPage {
 	
 	public RediffLoginPage(WebDriver driver) {
 		
-		this.driver = driver;		
+		this.driver = driver;	
+		PageFactory.initElements(driver, this);
+		
 	}
 	
 	
-	By username  = By.xpath("//*[@id='login1']");
-	By password = By.xpath("//*[@id='password']");
+	/*By username  = By.xpath("//*[@id='login1']");
+	By password = By.xpath("//*[@id='password']");*/
+	
+	@FindBy(xpath="//*[@id='login1']")
+	WebElement username;
+	
+	@FindBy(xpath="//*[@id='password']")
+	WebElement password;
 	
 	public WebElement UserName()
 	{
-		return driver.findElement(username);
+		//return driver.findElement(username);
+		return username;
 	}
 	
 	public WebElement Password()
 	{
-		return driver.findElement(password);
+		//return driver.findElement(password);
+		return password;
 	}
+	
 
 }
